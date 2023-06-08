0712913e6ab7eaaae66e8dc5209dda546ce4509c
Santhoshgithub22 <santhoshbharath61@gmail.com>
2023-06-08 12:41:09 +0530

    first end to end project

---
+++ README.md
@@ -0,0 +1,24 @@
+# END TO END MACHINE LEARNING PROJECT
+
+## DIAMOND PRICE PREDICTION
+
+### The very first step is we should create a separate virutual environment for this project by using this below command
+
+```
+conda create -p venv python==3.8
+
+conda activate venv/
+```
+
+### Then create a requirement text for to import all the required (necessary) libraries through this text file by using this below command
+
+```
+pip install -r requirements.txt
+```
+
+### To make a folders into package
+
+```
+from setuptools import find_packages, setup
+```
+
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPEN_E_DOT = "-e ."
+
+def get_requirements(filepath:str)->List[str]:
+
+    requirements = []
+    with open(filepath) as file_obj:
+        requirements = file_obj.readlines() # there /n is available for line by line, so we should replace it
+        requirements = [req.replace("\n", "") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements
+
+
+setup(
+    name = "Diamond Price Prediction Project",
+    version = "0.0.1",
+    author = "Santhosh Kumar",
+    author_email = "santhoshbharath61@gmail.com",
+    install_requires = get_requirements('requirements.txt'),
+    packages = find_packages()
+)
